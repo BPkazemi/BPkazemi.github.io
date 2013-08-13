048b7763d076be2fed00d168fa7fa39871b949cb
Babak Pourkazemi <bp5xj@virginia.edu>
2013-08-13 18:42:03 -0400

    Added transitions, fixed some hacky JS that changed CSS, added MOAR COLOR.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,29 @@ $(document).ready(function() {
 	// 	$('.title-menu').slideDown(500);
 	// })
 
+	$('.left').on('mouseover', function() {
+		$(this).css({
+			width: '35%',
+			opacity: 1
+		});
+	}).on('mouseout', function() {
+		$(this).css({
+			width: '29%',
+			opacity: 0.92
+		});
+	});
+
 	$('#about-tab, #projects-tab, #blog-tab, #bucketlist-tab').on('click', function() {
-			$('.tab').css({
-				'background': 'transparent',
-				'border': 'none',
-				'color': '#555'
+			// Add custom styling to selected tab
+			$('.tab').each(function() {
+				$(this).removeClass('selected');
 			});
+			$(this).addClass('selected');
 
-			$(this).css({
-				'background': '#BABABA',
-				'color': '#fff200'
-			});
-
+			// Hide all right side content
 			$('.tab-content').css('display', 'none');
+
+			// Show appropriate right side content
 			var tabName = $(this).get(0).id;
 			$('.' + tabName + '-wrapper').css('display', 'block')
 	})
