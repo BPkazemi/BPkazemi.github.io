37bf05d7b3c6419cac02e9217822408252c094b5
Babak Pourkazemi <bp5xj@virginia.edu>
2013-09-24 00:09:08 -0400

    temp removal of http://bpourkazemi.com in cname

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-$(document).ready(function() {
-	// $('.title.name').on('mouseover', function() {
-	// 	$('.title-menu').slideDown(500);
-	// })
+var app = angular.module('app', []);
 
-	$('.left').on('mouseover', function() {
-		$(this).css({
-			width: '32%',
-			opacity: 1
-		});
-	}).on('mouseout', function() {
-		$(this).css({
-			width: '27%',
-			opacity: 0.92
-		});
-	});
+app.controller('MainCtrl', function($scope) {
+	console.log('Main controller');
+	// $('.left').on('mouseover', function() {
+	// 	$(this).css({
+	// 		width: '32%',
+	// 		opacity: 1
+	// 	});
+	// }).on('mouseout', function() {
+	// 	$(this).css({
+	// 		width: '27%',
+	// 		opacity: 0.92
+	// 	});
+	// });
 
-	$('#about-tab, #projects-tab, #blog-tab, #bucketlist-tab').on('click', function() {
-			// Add custom styling to selected tab
-			$('.tab').each(function() {
-				$(this).removeClass('selected');
-			});
-			$(this).addClass('selected');
+	// $('#about-tab, #projects-tab, #blog-tab, #bucketlist-tab').on('click', function() {
+	// 		// Add custom styling to selected tab
+	// 		$('.tab').each(function() {
+	// 			$(this).removeClass('selected');
+	// 		});
+	// 		$(this).addClass('selected');
 
-			// Hide all right side content
-			$('.tab-content').css('display', 'none');
+	// 		// Hide all right side content
+	// 		$('.tab-content').css('display', 'none');
 
-			// Show appropriate right side content
-			var tabName = $(this).get(0).id;
-			$('.' + tabName + '-wrapper').css('display', 'block')
-	})
+	// 		// Show appropriate right side content
+	// 		var tabName = $(this).get(0).id;
+	// 		$('.' + tabName + '-wrapper').css('display', 'block')
+	// });
 });
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'MainCtrl',
+		templateUrl: './partials/about_me.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+})
